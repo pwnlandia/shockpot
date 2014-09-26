110f5ae0c358d123bf02bb6df7a0def542019330
Jason Trost <jason.trost@gmail.com>
2014-09-26 15:49:23 -0700

    execption handling to the requests.get calls

--- commands.py
+++ commands.py
@@ -20,6 +20,8 @@ def perform_commands(headers):
             count = min(20, int(ping.get('count', 1)))
             host = ping['host']
             LOGGER.info('Performing {} pings against {}'.format(count, host))
+
+            # host must match an IP regex and count must be a number, prevents command injection here
             command = 'ping -c {} {}'.format(count, host)
             os.system(command)
             return command, ''
@@ -29,16 +31,25 @@ def perform_commands(headers):
             wget = mat.groupdict()
             url = wget['url']
             LOGGER.info('Performing wget request on {}'.format(url))
-            resp = requests.get(url, headers={'User-Agent': 'Wget/1.13.4 (linux-gnu)'})
-            return 'wget {}'.format(url), resp.text
+            data = ''
+            try:
+                resp = requests.get(url, headers={'User-Agent': 'Wget/1.13.4 (linux-gnu)'})
+                data = resp.text
+            except Exception as e:
+                LOGGER.error(e)
+            return 'wget {}'.format(url), data
 
         mat = curl_check_re.search(value)
         if mat:
             curl = mat.groupdict()
             url = curl['url']
             LOGGER.info('Performing curl request on {}'.format(url))
-            resp = requests.get(url, headers={'User-Agent': 'curl/7.30.0'})
-            return 'curl {}'.format(url), resp.text
+            try:
+                resp = requests.get(url, headers={'User-Agent': 'curl/7.30.0'})
+                data = resp.text
+            except Exception as e:
+                LOGGER.error(e)
+            return 'curl {}'.format(url), data
 
         mat = telnet_check_re.search(value)
         if mat:
