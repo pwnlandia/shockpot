3743ed6d0e49afb80a3012ad90404573a770d7ff
Jason Trost <jason.trost@gmail.com>
2014-11-19 16:59:53 -0500

    added fetching of public IP for use in the logs

--- shockpot.py
+++ shockpot.py
@@ -6,9 +6,11 @@ from logger import LOGGER
 import bottle
 from bottle import request, response
 import json
-from util import get_hpfeeds_client
+from util import get_hpfeeds_client, get_ext_ip
 from commands import perform_commands
 import re
+import datetime
+import urlparse
 
 shellshock_re = re.compile(r'\(\s*\)\s*{')
 
@@ -24,6 +26,11 @@ LOGGER.info( 'Loading config file shockpot.conf ...')
 app.config.load_config('shockpot.conf')
 hpclient = get_hpfeeds_client(app.config)
 
+public_ip = None
+if app.config['fetch_public_ip.enabled'].lower() == 'true':
+    public_ip = get_ext_ip(json.loads(app.config['fetch_public_ip.urls']))
+    print 'public_ip =', public_ip
+
 def is_shellshock(headers):
     for name, value in headers:
         if shellshock_re.search(value):
@@ -38,6 +45,11 @@ def get_request_record():
     if is_shellshock_check:
         command, data = perform_commands(headers)
 
+    if public_ip:
+        dest_host = public_ip
+    else:
+        dest_host = urlparse.urlparse(request.url).netloc.split(':',1)[0]
+
     return {
         'method': request.method,
         'url': request.url,
@@ -46,10 +58,11 @@ def get_request_record():
         'headers': headers,
         'source_ip': request.environ.get('REMOTE_ADDR'),
         'dest_port': request.environ.get('SERVER_PORT'),
-        'dest_host': request.environ.get('SERVER_NAME'),
+        'dest_host': dest_host,
         'is_shellshock': is_shellshock_check,
         'command': command,
         'command_data': data,
+        'timestamp': str(datetime.datetime.now())
     }
 
 def log_request(record):
